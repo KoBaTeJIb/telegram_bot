ae1a0dd17b14de06ff05503432fccca41f07c375
Andrey Makimov <onixbed@gmail.com>
2017-09-11 14:41:53 +0600

    [*] Добавлена проверка конекта бд [*] каждая операция имеет свой экемпляр работы

--- src/main/java/ru/kobatejib/telegram/bot/bittrex/service/DataBaseService.java
+++ src/main/java/ru/kobatejib/telegram/bot/bittrex/service/DataBaseService.java
@@ -13,7 +13,6 @@ import ru.kobatejib.telegram.bot.bittrex.utility.DataBaseUtility;
 public class DataBaseService {
 
 	public Connection connection;
-	public Statement statement;
 
 	public void connect() throws ClassNotFoundException, SQLException {
 		try {
@@ -30,18 +29,26 @@ public class DataBaseService {
 	public void createDb() throws ClassNotFoundException, SQLException {
 
 		try {
-			if(connection != null){
-				statement = connection.createStatement();
-				statement.execute("CREATE TABLE if not exists 'bittrex' "
-						+ "('id' INTEGER PRIMARY KEY AUTOINCREMENT, "
-						+ "'order_type' text, " + "'order_uuid' text, "
-						+ "'exchange' text, " + "'quantity' text, "
-						+ "'price' text, " + "'opened' text, " + "'closed' text);");
-				System.out.println("Create table OK");
+			if (connection != null) {
+				Statement statement = null;
+				try {
+					statement = connection.createStatement();
+					statement.execute("CREATE TABLE if not exists 'bittrex' "
+							+ "('id' INTEGER PRIMARY KEY AUTOINCREMENT, "
+							+ "'order_type' text, " + "'order_uuid' text, "
+							+ "'exchange' text, " + "'quantity' text, "
+							+ "'price' text, " + "'opened' text, "
+							+ "'closed' text);");
+					System.out.println("Create table OK");
+				} finally {
+					if (statement != null) {
+						statement.close();
+					}
+				}
 			} else {
 				System.out.println("Error connection is null");
 			}
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
@@ -49,31 +56,56 @@ public class DataBaseService {
 
 	public void writeDb(Order order)
 			throws ClassNotFoundException, SQLException {
-		try {
-			statement.execute("INSERT INTO 'bittrex' "
-					+ "('order_type', 'order_uuid', 'exchange', 'quantity', 'price', 'opened', 'closed') "
-					+ "VALUES " + "(" + order.getOrderType() + ","
-					+ order.getOrderUuid() + "," + order.getExchange() + ","
-					+ order.getQuantity() + "," + order.getPrice() + ","
-					+ order.getOpened() + "," + order.getClass() + ","
-					+ order.getPrice() + ");");
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		if (connection != null) {
+			try {
+				Statement statement = null;
+				try {
+					statement = connection.createStatement();
+					statement.execute("INSERT INTO 'bittrex' "
+							+ "('order_type', 'order_uuid', 'exchange', 'quantity', 'price', 'opened', 'closed') "
+							+ "VALUES " + "(" + order.getOrderType() + ","
+							+ order.getOrderUuid() + "," + order.getExchange()
+							+ "," + order.getQuantity() + "," + order.getPrice()
+							+ "," + order.getOpened() + "," + order.getClass()
+							+ "," + order.getPrice() + ");");
+				} finally {
+					if (statement != null) {
+						statement.close();
+					}
+				}
+
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		} else {
+			System.out.println("Error connection is null");
 		}
+
 	}
 
 	public List<Order> findAllByClosed(Boolean closed)
 			throws ClassNotFoundException, SQLException {
 		List<Order> orders = new ArrayList<Order>();
 		String closedParam = DataBaseUtility.convertBooean2String(closed);
-		try {
-			ResultSet resultSet = statement.executeQuery(
-					"SELECT * FROM 'bittrex' WHERE closed=" + closedParam);
-			orders = DataBaseUtility.convertResultSet2ListOrders(resultSet);
-			System.out.println(resultSet);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		Statement statement = null;
+		if (connection != null) {
+			try {
+				statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(
+						"SELECT * FROM 'bittrex' WHERE closed=" + closedParam);
+				orders = DataBaseUtility.convertResultSet2ListOrders(resultSet);
+				System.out.println(resultSet);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			} finally {
+				if (statement != null) {
+					statement.close();
+				}
+			}
+		} else {
+			System.out.println("Error connection is null");
 		}
+
 		return orders;
 	}
 }
